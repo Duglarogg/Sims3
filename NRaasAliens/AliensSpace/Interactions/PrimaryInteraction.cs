f0c2c90572741d91e9507e62ef11f1c27efca647
Jonathan McRae <duglarogg@gmail.com>
2018-06-04 19:59:14 -0500

    Forgot Menu Interaction...Doh!
    - PrimaryInteraction: The main interaction through which to access the mod's menu.

---
+++ NRaasAliens/AliensSpace/Interactions/PrimaryInteraction.cs
@@ -0,0 +1,34 @@
+﻿using NRaas.CommonSpace.Interactions;
+using NRaas.CommonSpace.Options;
+using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Interfaces;
+using Sims3.SimIFace;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRaas.AliensSpace.Interactions
+{
+    public class PrimaryInteraction : ListedInteraction<IPrimaryOption<GameObject>, GameObject>
+    {
+        public static InteractionDefinition Singleton = new CommonDefinition<PrimaryInteraction>();
+
+        public override void AddInteraction(Common.InteractionInjectorList interactions)
+        {
+            interactions.AddRoot(Singleton);
+            interactions.Add<Sim>(Singleton);
+        }
+
+        protected override bool Test(IActor actor, GameObject target, GameObjectHit hit, ref GreyedOutTooltipCallback greyedOutTooltipCallback)
+        {
+            Sim targetSim = target as Sim;
+
+            if (targetSim != null)
+                return (target == actor);
+            else
+                return true;
+        }
+    }
+}
